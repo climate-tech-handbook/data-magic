17ba58c4af25ae042c7a30c11d6dc44bc2a9714d
futuresoup <email@example.com>
2023-04-30 20:01:10 -0700

    update code, generates files but content is wrong

--- content-generator/generate_markdown.py
+++ content-generator/generate_markdown.py
@@ -28,15 +28,35 @@ with open('file_info.csv', newline='') as f:
 with open('template.md') as f:
     template = f.read()
 
+# Generate content for a given prompt
+def generate_content(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=1024,
+        n = 1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    return response.choices[0].text
+
 # Loop over pages and generate output
 for page in file_info:
     # Generate output for current page
+    progress_made = generate_content(prompts['Progress Made']['prompt'].replace('{topic}', page['Topic']))
+    lessons_learned = generate_content(prompts['Lessons Learned']['prompt'].replace('{topic}', page['Topic']))
+    challenges_ahead = generate_content(prompts['Challenges Ahead']['prompt'].replace('{topic}', page['Topic']))
+    best_path_forward = generate_content(prompts['Best Path Forward']['prompt'].replace('{topic}', page['Topic']))
+
     output = template.format(
         topic=page['Topic'],
-        progress_made=prompts['Progress Made']['text'].replace('{topic}', page['Topic']),
-        lessons_learned=prompts['Lessons Learned']['text'].replace('{topic}', page['Topic']),
-        challenges_ahead=prompts['Challenges Ahead']['text'].replace('{topic}', page['Topic']),
-        best_path_forward=prompts['Best Path Forward']['text'].replace('{topic}', page['Topic'])
+        progress_made=progress_made,
+        lessons_learned=lessons_learned,
+        challenges_ahead=challenges_ahead,
+        best_path_forward=best_path_forward
     )
 
     # Write output to file
