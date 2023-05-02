e33dd938e974fd83f6c62cd4122081e89c7842f8
Howard Cao <60860365+howard989@users.noreply.github.com>
2023-05-02 10:56:44 -0700

    Update generate_markdown.py
    Update script to include pictures

--- content-generator/generate_markdown.py
+++ content-generator/generate_markdown.py
@@ -3,11 +3,13 @@ import os
 from dotenv import load_dotenv
 import csv
 import yaml
+import requests
 
 load_dotenv()
 
 # Get OpenAI API key
 api_key = os.getenv("OPENAI_API_KEY")
+unsplash_access_key = os.getenv("UNSPLASH_ACCESS_KEY")
 
 # Authenticate with OpenAI API key
 openai.api_key = api_key
@@ -42,6 +44,15 @@ def generate_content(prompt):
     )
     return response.choices[0].text.strip()
 
+# Fetch an image from Unsplash based on the topic
+def fetch_unsplash_image(topic):
+    response = requests.get(f"https://api.unsplash.com/search/photos?query={topic}&client_id={unsplash_access_key}")
+    data = response.json()
+    if data['results']:
+        return data['results'][0]['urls']['regular'], data['results'][0]['user']['links']['html']
+    else:
+        return None, None
+
 # Loop over pages and generate output
 for page in file_info:
     # Generate output for current page
@@ -50,18 +61,20 @@ for page in file_info:
     challenges_ahead = generate_content(prompts['Challenges Ahead']['prompt'].replace('{Topic}', page['Topic']))
     best_path_forward = generate_content(prompts['Best Path Forward']['prompt'].replace('{Topic}', page['Topic']))
 
-    # Populate the template with the generated content
+    # Fetch an image from Unsplash
+    image_url, credit_url = fetch_unsplash_image(page['Topic'])
+
+    # Populate the template with the generated content and image URL
     output = template.format(
         topic=page['Topic'],
         progress_made=progress_made,
         lessons_learned=lessons_learned,
         challenges_ahead=challenges_ahead,
-        best_path_forward=best_path_forward
+        best_path_forward=best_path_forward,
+        image_url=image_url or '',
+        credit_url=credit_url or ''
     )
 
     # Write output to file
     with open(f"output/{page['File Name']}", 'w') as f:
         f.write(output)
-
-
-
